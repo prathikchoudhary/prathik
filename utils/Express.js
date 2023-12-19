7d3dbe85582e5e58765643e9a0a3007e86ad2163
Prathik choudhary <136879319+prathikchoudhary@users.noreply.github.com>
2023-12-19 17:25:10 +0530

    Delete utils directory

--- utils/Express.js
+++
@@ -1,8 +0,0 @@
-class ExpressError extends Error {
-    constructor(stautsCode,message){
-        super();
-        this.stautsCode = stautsCode;
-        this.message = message;
-    }
-}
-module.exports = ExpressError;
